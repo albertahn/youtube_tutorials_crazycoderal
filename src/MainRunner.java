3ba4e0b3206b8fc2ec10fe6b175139934c416cbd
Albert <contendera@gmail.com>
2017-09-09 11:34:05 -0700

    init

---
+++ src/MainRunner.java
@@ -0,0 +1,15 @@
+public class MainRunner {
+
+    //https://www.hackerrank.com/challenges/ctci-array-left-rotation
+
+
+    public static void main(String[] args){
+
+        System.out.println("hi there");
+
+
+    }//main
+
+
+
+}//class
